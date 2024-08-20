262819589fb6fc1bc92b9aab8c8133d19b53de0f
avkarimov <karimov3266@yandex.ru>
2024-08-21 01:35:44 +0500

    yaml

--- tests/test_go_to_pages.py
+++ tests/test_go_to_pages.py
@@ -10,7 +10,7 @@ from playwright.sync_api._generated import Page
 from pages.webinars_page import WebinarsPage
 
 
-@allure.suite("Проверка ссылок на главной страницы Иннополиса")
+@allure.suite("Проверка ссылок на главной странице Иннополиса")
 class TestGoPagesFromMain:
     @allure.title("Успешный переход в раздел Отзыв из меню главной страницы")
     def test_open_feedback(self, browser_page: Page, main_page: MainPage):
@@ -18,7 +18,7 @@ class TestGoPagesFromMain:
             browser_page.goto(main_page.url)
         with allure.step(f"Перейти в раздел {main_page.feedback}, и проверить что URL раздела корректный"):
             page_feedback = main_page.click_feedback(browser_page=browser_page).value
-            time.sleep(5)
+            time.sleep(10)
             assert page_feedback.url == FeedbackPage.url, f"Некорректный URL в разделе {main_page.feedback}"
         with allure.step(f"Проверить, что на странице есть заголовок {FeedbackPage.heading}"):
             heading_on_page = get_value_h1(page=page_feedback)
@@ -30,7 +30,7 @@ class TestGoPagesFromMain:
             browser_page.goto(MainPage.url)
         with allure.step(f"Перейти в раздел {main_page.blog}, и проверить что URL раздела корректный"):
             page_blog = main_page.click_blog(browser_page=browser_page).value
-            time.sleep(5)
+            time.sleep(10)
             assert page_blog.url == BlogPage.url, f"Некорректный URL в разделе {main_page.blog}"
         with allure.step(f"Проверить, что на странице есть заголовок {BlogPage.heading}"):
             heading_on_page = get_value_h1(page=page_blog)
@@ -42,7 +42,7 @@ class TestGoPagesFromMain:
             browser_page.goto(MainPage.url)
         with allure.step(f"Перейти в раздел {main_page.webinars}, и проверить что URL раздела корректный"):
             page_webinars = main_page.click_webinars(browser_page=browser_page).value
-            time.sleep(5)
+            time.sleep(10)
             assert page_webinars.url == WebinarsPage.url, f"Некорректный URL в разделе {main_page.webinars}"
         with allure.step(f"Проверить, что на страница есть заголовок {WebinarsPage.heading}"):
             heading_on_page = get_value_h1(page=page_webinars)
